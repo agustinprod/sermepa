c419e27f1199907525b918e6fab8c21d361358ec
Agustín Rodríguez <killgt@gmail.com>
2016-11-10 13:58:58 +0100

    allow null on exceptions

--- src/Killgt/Sermepa/Exceptions/CallbackErrorException.php
+++ src/Killgt/Sermepa/Exceptions/CallbackErrorException.php
@@ -3,7 +3,7 @@
 class CallbackErrorException extends \Exception {
     private $parameters = array();
 
-    public function __construct($message, $code, array $parameters = array()) {
+    public function __construct($message, $code, $parameters = array()) {
         parent::__construct($message, $code);
         $this->parameters = $parameters;
     }
